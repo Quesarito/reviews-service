f094e30643c4332b56d0b40230995c5bfd60aa9e
klarok <karaylu@gmail.com>
2018-12-20 22:27:30 -0800

    Server now takes requests with parameter productId
    	- Minor edits to seedData to conform with linter
    	- Added feature ratings, reviews.* to database query

--- database/index.js
+++ database/index.js
@@ -10,13 +10,14 @@ db.connect();
 //Takes an object stating the search criteria
 const retrieve = (productId, callback) => {
   //product id as search criteria
-  let query = `SELECT sub.*, media.file 
-  FROM (SELECT products.name AS productName, 
-    reviews.headline AS headline, 
-    reviews.id AS reviewId FROM products, reviews 
+  let query = `SELECT revs.*, media.file, 
+    ratings_feature.feature, ratings_feature.rating AS featureRating 
+  FROM (SELECT products.name AS productName,
+    reviews.* FROM products, reviews 
     WHERE products.id = reviews.product_id 
-    AND reviews.product_id = ?) sub 
-  LEFT JOIN media ON media.review_id = sub.reviewId;
+    AND reviews.product_id = ?) revs 
+  LEFT JOIN media ON media.review_id = revs.id
+  LEFT JOIN ratings_feature ON ratings_feature.product_id = revs.product_id;
     `;
   db.query(query, productId, (err, results) => {
     if (err) { throw err; }
--- server/index.js
+++ server/index.js
@@ -2,16 +2,18 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = 3000;
-const {db, retrieve} = require('../database');
+const db = require('../database');
 
 app.use(express.static('public'));
 app.use(bodyParser());
 
-app.get('/reviews', (req, res) => {
-  retrieve(41, (err, results) => {
+app.get('/reviews/:productId', (req, res) => {
+  db.retrieve(req.params.productId, (err, results) => {
+    if (err) {
+      res.status(500).send(err);
+    }
     res.send(results);
   });
-  
 });
 
 app.listen(port, (err) => {
