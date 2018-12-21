4fcc1e9f86d95a85209ba9a7ef4f49080ef13cfe
klarok <karaylu@gmail.com>
2018-12-20 19:57:28 -0800

    Can receive and process request for specific product reviews
    	- Product id hardcoded. TODO: send product id in GET request
    	- Added webpack compiled js file to gitignore (same as on views branch)

--- database/index.js
+++ database/index.js
@@ -1,11 +1,29 @@
 const mysql = require('mysql');
 const db = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	database: 'amazon_reviews'
+  host: 'localhost',
+  user: 'root',
+  database: 'amazon_reviews'
 });
 
 db.connect();
 
+//Takes an object stating the search criteria
+const retrieve = (productId, callback) => {
+  //product id as search criteria
+  let query = `SELECT sub.*, media.file 
+  FROM (SELECT products.name AS productName, 
+    reviews.headline AS headline, 
+    reviews.id AS reviewId FROM products, reviews 
+    WHERE products.id = reviews.product_id 
+    AND reviews.product_id = ?) sub 
+  LEFT JOIN media ON media.review_id = sub.reviewId;
+    `;
+  db.query(query, productId, (err, results) => {
+    if (err) { throw err; }
+    console.log(results);
+    callback(null, results);
+  });
+};
+
 exports.db = db;
-exports.mysql = mysql;
+exports.retrieve = retrieve;
--- server/index.js
+++ server/index.js
@@ -2,16 +2,25 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = 3000;
+const {db, retrieve} = require('../database');
 
-app.use(express.static('../public'));
-
+app.use(express.static('public'));
 app.use(bodyParser());
 
+// app.get('/', (req, res) => {
+//   res.send('hihi');
+// });
+
 app.get('/reviews', (req, res) => {
-	res.send('hi');
+  retrieve(41, (err, results) => {
+    res.send(results);
+  });
+  
 });
 
 app.listen(port, (err) => {
-	if (err) throw err;
-	console.log('Now listening on port ' + port);
+  if (err) {
+    throw err;
+  }
+  console.log('Now listening on port ' + port);
 });
